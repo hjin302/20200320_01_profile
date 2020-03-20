2b448edd2e5cb7468a345df8a142ce08ba330fcf
hjin302 <enro302@naver.com>
2020-03-20 10:58:40 +0900

    프로필 사진 누르면 => 사진 선택 화면으로 이동

--- app/src/main/java/com/example/a20200320_01_profile/BaseActivity.java
+++ app/src/main/java/com/example/a20200320_01_profile/BaseActivity.java
@@ -3,7 +3,6 @@ import android.content.Context;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-// abstract 메쏘드를 가지는 클래스는 반드시 abstract class
 public abstract class BaseActivity extends AppCompatActivity {
 
     public Context mContext = this;
--- app/src/main/java/com/example/a20200320_01_profile/MainActivity.java
+++ app/src/main/java/com/example/a20200320_01_profile/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.a20200320_01_profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a20200320_01_profile.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("화면 재등장", "다시 나타날때마다 실행");
+    }
+
+    @Override
+    public void setupEvents() {
+    binding.profilePicBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+//            갤러리에서 사진을 가져오자!
+//            안드로이드가 제공하는 화면(갤러리) => Intent로 이동.
+
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            startActivity(intent);
+        }
+    });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+
 }
