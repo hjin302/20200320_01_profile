9fd90cf3f8681842b08fccac92dc5ca29bd13180
hjin302 <enro302@naver.com>
2020-03-20 11:17:12 +0900

    선택한 이미지 경로 로그로 찍기까지.

--- app/src/main/java/com/example/a20200320_01_profile/MainActivity.java
+++ app/src/main/java/com/example/a20200320_01_profile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a20200320_01_profile;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -12,6 +13,8 @@ import android.view.View;
 import com.example.a20200320_01_profile.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    private static final int REQUEST_FOR_ALBUM = 1000;
 
     ActivityMainBinding binding = null;
 
@@ -40,7 +43,7 @@ public class MainActivity extends BaseActivity {
             Intent intent = new Intent(Intent.ACTION_PICK);
             intent.setType("image/*");
             intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-            startActivity(intent);
+            startActivityForResult(intent, REQUEST_FOR_ALBUM);
         }
     });
     }
@@ -50,5 +53,16 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode == REQUEST_FOR_ALBUM){
+            if(resultCode == RESULT_OK){
+                if(data.getData() != null){
+                    Log.d("사진가져오기", data.getData().toString());
+                }
+            }
+        }
+    }
 }
